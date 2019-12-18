43e5a0a5a901218331972822ce81d9bebe158c4b
likuan66 <1402424218@qq.com>
2019-12-18 08:04:19 +0800

    practice

---
+++ 课堂代码/初阶面向对象/demo03/DemoMath.java
@@ -0,0 +1,10 @@
+package demo02;
+
+public class DemoMath {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-3.14));//获取绝对值
+        System.out.println(Math.ceil(-3.6));//向上取整
+        System.out.println(Math.floor(-4.5));//向下取整
+        System.out.println(Math.round(4.2563));//四舍五入
+    }
+}
