6dff34f188c7bf0a2a33361206f6f8ba23ed7dbf
likuan66 <1402424218@qq.com>
2019-12-23 11:08:50 +0800

    practice

---
+++ 课堂代码/初阶面向对象/demo05/MyInterfaceAbstract.java
@@ -0,0 +1,12 @@
+package demo04;
+
+public interface MyInterfaceAbstract {
+    //定义了四个抽象方法
+    public abstract void methodAbs1();
+
+    abstract void methodAbs2();
+
+    public void methodAbs3();
+
+    void methodAbs4();
+}
