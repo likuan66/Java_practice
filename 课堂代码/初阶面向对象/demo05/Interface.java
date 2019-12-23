a035e1782ce63c2c6d7f18e4e2f0182822687485
likuan66 <1402424218@qq.com>
2019-12-23 11:08:33 +0800

    practice

---
+++ 课堂代码/初阶面向对象/demo05/Interface.java
@@ -0,0 +1,10 @@
+package demo04;
+
+public class Interface {
+    public static void main(String[] args) {
+        //创建实现类对象使用
+        MyInterfaceAbstractImpl impl=new MyInterfaceAbstractImpl();
+        impl.methodAbs1();
+        impl.methodAbs2();
+    }
+}
