f87b32c0ed798864138c8cf0752088c090fd41a2
likuan66 <1402424218@qq.com>
2019-12-28 13:49:25 +0800

    practice

---
+++ 课堂代码/初阶面向对象/demo07/Myred.java
@@ -0,0 +1,13 @@
+package demo06;
+import red.RedPacketFrame;
+
+public class Myred extends RedPacketFrame{
+     /**
+     * 构造方法：生成红包界面。
+     *
+     * @param title 界面的标题
+     */
+    public Myred(String title) {
+        super(title);
+    }
+}
