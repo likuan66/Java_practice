8ad24b908659893dd97facb18a1aef3323ba6a76
likuan66 <1402424218@qq.com>
2020-02-03 18:02:27 +0800

    practice

---
+++ 课堂代码/RegisterException.java
@@ -0,0 +1,11 @@
+package RegisterException;
+
+public class RegisterException extends /*Exception */RuntimeException{
+    public RegisterException() {
+        super();
+    }
+
+    public RegisterException(String message) {
+        super(message);
+    }
+}
