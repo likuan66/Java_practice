0396f2f763931837af9c912aac1f756c590a3252
likuan66 <1402424218@qq.com>
2020-02-14 13:32:37 +0800

    practice

---
+++ biteHomework/addBinaryPractice.java
@@ -0,0 +1,34 @@
+package day05;
+
+public class addBinaryPractice {
+    public static void main(String[] args) {
+        String s1="11";
+        String s2="1";
+        System.out.println(addBinary(s1,s2));
+
+    }
+    public static String addBinary(String a, String b) {
+        if(a==null||a.length()==0){
+            return b;
+        }
+        if(b==null||b.length()==0){
+            return a;
+        }
+        int i=a.length()-1;
+        int j=b.length()-1;
+        StringBuilder stb=new StringBuilder();
+        int c=0;
+        while(i>=0||j>=0){
+            if(i>=0){
+                c+=a.charAt(i--)-'0';
+            }
+            if(j>=0){
+                c+=b.charAt(j--)-'0';
+            }
+            stb.append(c%2);
+            c>>=1;
+        }
+        String res=stb.reverse().toString();
+        return c>0?'1'+res:res;
+    }
+}
