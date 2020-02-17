9103f36c9813d4c6f10046adc621f0457fe03098
likuan66 <1402424218@qq.com>
2020-02-17 20:57:50 +0800

    practice

---
+++ biteHomework/evalRPNPractice.java
@@ -0,0 +1,34 @@
+package day06;
+
+public class evalRPNPractice {
+    public static void main(String[] args) {
+        String[] arr={"2","1","+","3","*"};
+        System.out.println(evalRPN(arr));
+    }
+    public static int evalRPN(String[] tokens) {
+        int[] numStack = new int[tokens.length / 2 + 1];
+        int index = 0;
+        for (String s : tokens) {
+            switch (s) {
+                case "+":
+                    numStack[index - 2] += numStack[--index];
+                    break;
+                case "-":
+                    numStack[index - 2] -= numStack[--index];
+                    break;
+                case "*":
+                    numStack[index - 2] *= numStack[--index];
+                    break;
+                case "/":
+                    numStack[index - 2] /= numStack[--index];
+                    break;
+                default:
+                    // numStack[index++] = Integer.valueOf(s);
+                    //valueOf改为parseInt，减少自动拆箱装箱操作
+                    numStack[index++] = Integer.parseInt(s);
+                    break;
+            }
+        }
+        return numStack[0];
+    }
+}
