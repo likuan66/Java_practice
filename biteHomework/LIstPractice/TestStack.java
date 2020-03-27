44ac3df8ef839a1c3a5c215c8c45baf2951ec2ce
likuan66 <1402424218@qq.com>
2020-03-27 14:22:32 +0800

    practice

---
+++ biteHomework/LIstPractice/TestStack.java
@@ -0,0 +1,23 @@
+package biteHomework.ListPractice;
+
+import java.util.Stack;
+
+public class TestStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        int ret=stack.pop();
+        System.out.println(ret);
+        ret=stack.pop();
+        System.out.println(ret);
+        ret=stack.pop();
+        System.out.println(ret);
+        ret=stack.pop();
+        System.out.println(ret);
+        ret=stack.pop();
+        System.out.println(ret);
+    }
+}
