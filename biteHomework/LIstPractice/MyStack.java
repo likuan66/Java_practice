344babb2aa445bcaef92131f96ee48a84ce2c668
likuan66 <1402424218@qq.com>
2020-03-27 14:21:46 +0800

    practice

---
+++ biteHomework/LIstPractice/MyStack.java
@@ -0,0 +1,22 @@
+package biteHomework.ListPractice;
+
+public class MyStack {
+    private int[] array=new int[100];
+    private int size=0;//栈中存放多少个元素
+
+    //入栈
+    public void push(int x){
+        array[size]=x;
+        size++;
+    }
+    //取栈顶元素（最后进来的元素）
+    public int peek(){
+        return array[size-1];
+    }
+    //出栈
+    public int pop(){
+        int ret=array[size-1];
+        size--;
+        return ret;
+    }
+}
