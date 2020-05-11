a8832a0f36cddf3df38ee0d6d01bba2b98a6521e
likuan66 <1402424218@qq.com>
2020-05-11 09:26:15 +0800

    practice

---
+++ 每日一题/day01.java
@@ -0,0 +1,37 @@
+package 每日一题;
+//实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+import java.util.Scanner;
+
+public class day01 {
+        public static double quickMul(double x, long N) {
+            double ans = 1.0;
+            // 贡献的初始值为 x
+            double x_contribute = x;
+            // 在对 N 进行二进制拆分的同时计算答案
+            while (N > 0) {
+                if (N % 2 == 1) {
+                    // 如果 N 二进制表示的最低位为 1，那么需要计入贡献
+                    ans *= x_contribute;
+                }
+                // 将贡献不断地平方
+                x_contribute *= x_contribute;
+                // 舍弃 N 二进制表示的最低位，这样我们每次只要判断最低位即可
+                N /= 2;
+            }
+            return ans;
+        }
+
+        public static double myPow(double x, int n) {
+            long N = n;
+            return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+        }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double x=sc.nextDouble();
+        int n=sc.nextInt();
+        double sum=myPow(x,n);
+        System.out.println(sum);
+    }
+}
